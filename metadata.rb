c2f7ac0895d0c37772ebb0881da18e00b1483732
Tim Smith <tim@cozy.co>
2015-08-21 11:49:45 -0700

    Add oracle and amazon linux to the metadata

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs vim and optional extra packages.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.3'
 
-%w(debian ubuntu arch redhat centos fedora scientific).each do |os|
+%w(debian ubuntu arch redhat centos fedora scientific oracle amazon).each do |os|
   supports os
 end
 

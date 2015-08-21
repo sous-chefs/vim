a704cba7c94d82291d76ffdb5535383c7dfede97
Tim Smith <tim@cozy.co>
2015-08-21 11:40:39 -0700

    Add new metadata and update Opscode -> Chef

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,14 @@
-name             "vim"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs vim and optional extra packages."
+name 'vim'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs vim and optional extra packages.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.3"
+version '1.1.3'
 
-%w{debian ubuntu arch redhat centos fedora scientific}.each do |os|
+%w(debian ubuntu arch redhat centos fedora scientific).each do |os|
   supports os
 end
 
+source_url 'https://github.com/opscode-cookbooks/vim' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/vim/issues' if respond_to?(:source_url)

4316aee1d37cdc61273a0fc6f6dfa8949d42b700
Tim Smith <tsmith84@gmail.com>
2015-10-01 14:58:20 -0700

    Workaround VIM not handling the new location of xsubpp on RHEL7/Fedora
    Symlink to the old location if on Fedora or RHEL 7+.  Don't perform the symlink if the original exists.  This allows users to override the packages to be installed to not include any perl packages without breaking this compile.

--- recipes/source.rb
+++ recipes/source.rb
@@ -26,6 +26,14 @@ node['vim']['source']['dependencies'].each do |dependency|
   end
 end
 
+# vim looks for xsubpp in wrong location RHEL 7+ and Fedora
+if node.platform? 'fedora' || (node.platform_family? 'rhel' && node.platform_version.to_i >= 7)
+  link '/usr/share/perl5/ExtUtils/xsubpp' do
+    to '/usr/bin/xsubpp'
+    only_if { ::File.exist?('/usr/bin/xsubpp') } # if package node attributes don't include perl this won't be here
+  end
+end
+
 remote_file "#{cache_path}/vim-#{source_version}.tar.bz2" do
   source "http://ftp.vim.org/pub/vim/unix/vim-#{source_version}.tar.bz2"
   checksum node['vim']['source']['checksum']

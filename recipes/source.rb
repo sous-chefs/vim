d2b5ba394b222fd0c489bcdc3d68f986f4b9a6e5
Tim Smith <tsmith84@gmail.com>
2015-10-01 13:17:02 -0700

    Remove alignment

--- recipes/source.rb
+++ recipes/source.rb
@@ -17,8 +17,8 @@
 # limitations under the License.
 #
 
-cache_path            = Chef::Config['file_cache_path']
-source_version        = node['vim']['source']['version']
+cache_path = Chef::Config['file_cache_path']
+source_version = node['vim']['source']['version']
 
 node['vim']['source']['dependencies'].each do |dependency|
   package dependency do

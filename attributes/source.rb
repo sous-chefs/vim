ac32590fe9c18ef70437940a3cb77f00eaf7d257
Tim Smith <tsmith84@gmail.com>
2015-10-01 23:10:23 -0700

    Make sure centos 6 gets the right packages and fix formatting

--- attributes/source.rb
+++ attributes/source.rb
@@ -23,42 +23,42 @@ default['vim']['source']['prefix']        = '/usr/local'
 default['vim']['source']['configuration'] = "--without-x --enable-pythoninterp --enable-rubyinterp --enable-tclinterp --enable-luainterp --enable-perlinterp --enable-cscope  --with-features=huge --prefix=#{default['vim']['source']['prefix']}"
 
 if platform_family? 'rhel', 'fedora'
-  if node['platform_version'].to_i > 6
-    default['vim']['source']['dependencies']  = %w( ctags
-                                                    gcc
-                                                    lua-devel
-                                                    luajit-devel
-                                                    make
-                                                    ncurses-devel
-                                                    perl-devel
-                                                    perl-ExtUtils-CBuilder
-                                                    perl-ExtUtils-Embed
-                                                    perl-ExtUtils-ParseXS
-                                                    python-devel
-                                                    ruby-devel
-                                                    tcl-devel
-                                                )
+  if node['platform_version'].to_i >= 6
+    default['vim']['source']['dependencies'] = %w( ctags
+                                                   gcc
+                                                   lua-devel
+                                                   luajit-devel
+                                                   make
+                                                   ncurses-devel
+                                                   perl-devel
+                                                   perl-ExtUtils-CBuilder
+                                                   perl-ExtUtils-Embed
+                                                   perl-ExtUtils-ParseXS
+                                                   python-devel
+                                                   ruby-devel
+                                                   tcl-devel
+                                               )
   else # centos 5 and earlier lack lua, luajit, and many of the perl packages found in later releases.  Also installs fail without libselinux-devel
-      default['vim']['source']['dependencies']  = %w( ctags
-                                                    gcc
-                                                    make
-                                                    ncurses-devel
-                                                    perl
-                                                    python-devel
-                                                    ruby-devel
-                                                    tcl-devel
-                                                    libselinux-devel
-                                                )
+    default['vim']['source']['dependencies'] = %w( ctags
+                                                   gcc
+                                                   make
+                                                   ncurses-devel
+                                                   perl
+                                                   python-devel
+                                                   ruby-devel
+                                                   tcl-devel
+                                                   libselinux-devel
+                                               )
   end
 else
-  default['vim']['source']['dependencies']  = %w( exuberant-ctags
-                                                  gcc
-                                                  libncurses5-dev
-                                                  libperl-dev
-                                                  lua5.1
-                                                  make
-                                                  python-dev
-                                                  ruby-dev
-                                                  tcl-dev
-                                              )
+  default['vim']['source']['dependencies'] = %w( exuberant-ctags
+                                                 gcc
+                                                 libncurses5-dev
+                                                 libperl-dev
+                                                 lua5.1
+                                                 make
+                                                 python-dev
+                                                 ruby-dev
+                                                 tcl-dev
+                                             )
 end

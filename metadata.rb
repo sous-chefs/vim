dc743f9905940eb146532d55d7f9e68ac617b2fa
Tim Smith <tsmith@chef.io>
2016-08-30 13:18:02 -0700

    Add chef_version to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs vim and optional extra packages.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.1'
+version '2.0.2'
 
 %w(debian ubuntu arch redhat centos fedora scientific oracle amazon zlinux).each do |os|
   supports os
@@ -12,3 +12,5 @@ end
 
 source_url 'https://github.com/chef-cookbooks/vim'
 issues_url 'https://github.com/chef-cookbooks/vim/issues'
+
+chef_version '>= 12.1' if respond_to?(:chef_version)

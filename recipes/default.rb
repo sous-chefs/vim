290dcc82794a3ac9d9290d460f5979e6aab975e4
Tim Smith <tsmith@chef.io>
2016-11-25 15:02:29 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: vim
+# Cookbook:: vim
 # Recipe:: default
 #
-# Copyright 2010-2016, Chef Software, Inc.
+# Copyright:: 2010-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
